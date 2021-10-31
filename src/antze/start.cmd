078f0ea89a4d63f6bbe7d49fb06206c89ebed1b9
Sabane Hosen <shosen@vigilantglobal.com>
2021-10-31 14:51:18 +0000

    adding start,batch files for windows

---
+++ src/antze/start.cmd
@@ -0,0 +1,8 @@
+@echo off
+:: BB Guild
+:: https://gameinfo.albiononline.com/api/gameinfo/guilds/yM8btgK-S06oqoOPp5Ewow
+set AO_KILLBOARD_GUILD=yM8btgK-S06oqoOPp5Ewow
+set AO_KILLBOARD_TOKEN=NzQ5MzY4ODMwMTkwNjgyMjMy.X0q-Gw.2fr7N2JHpSdtU6-kYYUgAwH7aDY
+set AO_KILLBOARD_CHANNEL=897891122402164766
+::python ao_killboard.py  --debug %*
+python ao_killboard.py  %*
